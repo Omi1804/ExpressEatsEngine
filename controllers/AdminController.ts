2c6f6af06b463ff7643fd68922a382671300acbd
Om Nigam <om2021064@akgec.ac.in>
2024-01-07 19:03:31 +0530

    first checkpoint

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -1,10 +1,54 @@
 import { Request, Response, NextFunction } from "express";
+import { CreateVendorInput } from "../dto/Vandor.dto";
+import { Vandor } from "../models";
+import { GeneratePassword, GenerateSalt } from "../utility";
 
 export const CreateVendor = async (
   req: Request,
   res: Response,
   next: NextFunction
-) => {};
+) => {
+  const {
+    name,
+    address,
+    pincode,
+    foodType,
+    email,
+    password,
+    ownerName,
+    phone,
+  } = <CreateVendorInput>req.body;
+
+  const existingVandor = await Vandor.findOne({ email: email });
+
+  if (existingVandor !== null) {
+    return res.json({ message: "A vandor is exist with this email ID" });
+  }
+
+  //generating salt
+
+  const salt = await GenerateSalt();
+  const userPassword = await GeneratePassword(password, salt);
+
+  //encrypting the pass using the salt
+
+  const createdVandor = await Vandor.create({
+    name: name,
+    address: address,
+    pincode: pincode,
+    foodType: foodType,
+    email: email,
+    password: userPassword,
+    salt: salt,
+    ownerName: ownerName,
+    phone: phone,
+    rating: 0,
+    serviceAvailable: false,
+    coverImages: [],
+  });
+
+  return res.json(createdVandor);
+};
 
 export const GetVendors = async (
   req: Request,
--- index.ts
+++ index.ts
@@ -1,10 +1,32 @@
 import express from "express";
+import bodyParser from "body-parser";
 import { AdminRoute, VandorRoute } from "./routes";
+import mongoose from "mongoose";
+const MONGO_URI =
+  process.env.MONGO_URI ||
+  "mongodb://localhost:27017/online_food_delivery_services";
+require("dotenv").config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/admin", AdminRoute);
 app.use("/vandor", VandorRoute);
+
+if (!MONGO_URI) {
+  console.log("please define a MongoDB connection");
+  process.exit(1);
+}
+
+mongoose
+  .connect(MONGO_URI, {
+    dbName: "online_food_delivery_services",
+  })
+  .then((result) => {
+    console.log("DB connected");
+  })
+  .catch((err) => console.log("error" + err));
 
 app.listen(8000, () => {
   console.log("App is listening on port 8000");
